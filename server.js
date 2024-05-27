f9da5838d670089308daec27af1525b35b736262
chimdykekamsi <ckamsi04@gmail.com>
2024-05-27 13:00:20 +0100

    setting up

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 
+app.get("health_check", (req,res) => {
+    res.status(200).json({
+        status: "OK"
+    });
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
